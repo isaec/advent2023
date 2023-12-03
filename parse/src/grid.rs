7e67d249148df54d5f1f7c809b0917cf1190e609
Isaac <isaac@rubenfamily.com>
2023-12-03 00:18:18 -0600

    start on parsing library

---
+++ parse/src/grid.rs
@@ -0,0 +1,7 @@
+use miette::Result;
+
+pub struct Grid {}
+
+pub fn parse_grid(input: &str) -> Result<Grid> {
+    todo!()
+}
---
+++ parse/src/lib.rs
@@ -0,0 +1,4 @@
+mod grid;
+
+pub use grid::parse_grid;
+pub use grid::Grid;
