bff8acb75a994adaa58533d54a54bde43c311dff
Isaac <isaac@rubenfamily.com>
2023-12-05 23:19:53 -0600

    do a bad job of day6

--- day6/src/part1.rs
+++ day6/src/part1.rs
@@ -1,3 +1,5 @@
+use std::iter::zip;
+
 use miette::Result;
 use miette_pretty::Pretty;
 use parse::QuickRegex;
@@ -7,13 +9,37 @@ fn main() {
     dbg!(part1(input).unwrap());
 }
 
-fn parse(input: &str) -> Result<Vec<&str>> {
-    input.lines().map(|l| Ok(l)).collect()
+fn parse(input: &str) -> Result<Vec<(i64, i64)>> {
+    let digits: Vec<Vec<i64>> = input
+        .lines()
+        .map(|l| l.get_digits())
+        .collect::<Result<_>>()?;
+
+    Ok(zip(digits[0].clone(), digits[1].clone()).collect())
+}
+
+fn find_choices(race: (i64, i64)) -> Vec<i64> {
+    let (time, record) = race;
+    (1..time)
+        .filter_map(|hold_time| {
+            let distance = hold_time * (time - hold_time);
+            dbg!(hold_time);
+            dbg!(distance);
+            if distance > record {
+                Some(hold_time)
+            } else {
+                None
+            }
+        })
+        .collect()
 }
 
 pub fn part1(input: &str) -> Result<i64> {
-    let parsed = parse(input)?;
-    Ok(0)
+    let races = parse(input)?;
+    Ok(races
+        .iter()
+        .map(|r| find_choices(*r).len() as i64)
+        .product())
 }
 
 #[cfg(test)]
@@ -24,9 +50,10 @@ mod part1_tests {
     #[test]
     fn example() {
         let input = indoc! {r#"
-
+Time:      7  15   30
+Distance:  9  40  200
 "#};
-        assert_eq!(part1(input).expect("part1 should return Ok"), 0);
+        assert_eq!(part1(input).expect("part1 should return Ok"), 288);
     }
 
     #[test]
--- day6/src/part2.rs
+++ day6/src/part2.rs
@@ -1,19 +1,44 @@
+use std::iter::zip;
+
 use miette::Result;
 use miette_pretty::Pretty;
 use parse::QuickRegex;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 fn main() {
     let input = include_str!("../input.txt");
     dbg!(part2(input).unwrap());
 }
 
-fn parse(input: &str) -> Result<Vec<&str>> {
-    input.lines().map(|l| Ok(l)).collect()
+fn parse(input: &str) -> Result<(i64, i64)> {
+    let digits: Vec<i64> = input
+        .lines()
+        .map(|l| l.get_digits()?.first().pretty().map(|d| *d))
+        .collect::<Result<_>>()?;
+
+    Ok((digits[0], digits[1]))
 }
 
-pub fn part2(input: &str) -> Result<i64> {
-    let parsed = parse(input)?;
-    Ok(0)
+fn find_choices(race: (i64, i64)) -> usize {
+    let (time, record) = race;
+    (1..time)
+        .into_par_iter()
+        .filter_map(|hold_time| {
+            let distance = hold_time * (time - hold_time);
+            // dbg!(hold_time);
+            // dbg!(distance);
+            if distance > record {
+                Some(hold_time)
+            } else {
+                None
+            }
+        })
+        .count()
+}
+
+pub fn part2(input: &str) -> Result<usize> {
+    let races = parse(input)?;
+    Ok(find_choices(races))
 }
 
 #[cfg(test)]
@@ -24,9 +49,10 @@ mod part2_tests {
     #[test]
     fn example() {
         let input = indoc! {r#"
-
+Time:      71530
+Distance:  940200
 "#};
-        assert_eq!(part2(input).expect("part2 should return Ok"), 0);
+        assert_eq!(part2(input).expect("part2 should return Ok"), 71503);
     }
 
     #[test]
