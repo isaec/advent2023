fcd1713302546c6270802cfbca27fe7d3d7f3678
Isaac <isaac@rubenfamily.com>
2023-12-08 01:04:09 -0600

    avoid tracing in release mode

--- miette_pretty/src/lib.rs
+++ miette_pretty/src/lib.rs
@@ -7,6 +7,7 @@ pub trait Pretty<T, E> {
     fn pretty_msg(self, msg: impl Display) -> Result<T, Report>;
 }
 
+#[cfg(debug_assertions)]
 impl<T, E: std::error::Error + Send + Sync + 'static> Pretty<T, E> for Result<T, E> {
     #[track_caller]
     fn pretty_msg(self, msg: impl Display) -> Result<T, Report> {
@@ -21,6 +22,7 @@ impl<T, E: std::error::Error + Send + Sync + 'static> Pretty<T, E> for Result<T,
     }
 }
 
+#[cfg(debug_assertions)]
 impl<T> Pretty<T, Report> for Option<T> {
     #[track_caller]
     fn pretty_msg(self, msg: impl Display) -> Result<T, Report> {
@@ -36,5 +38,27 @@ impl<T> Pretty<T, Report> for Option<T> {
             "None at {}",
             std::panic::Location::caller()
         )))
+    }
+}
+
+#[cfg(not(debug_assertions))]
+impl<T, E: std::error::Error + Send + Sync + 'static> Pretty<T, E> for Result<T, E> {
+    fn pretty_msg(self, msg: impl Display) -> Result<T, Report> {
+        self.into_diagnostic().wrap_err(format!("{msg}"))
+    }
+
+    fn pretty(self) -> Result<T, Report> {
+        self.into_diagnostic()
+    }
+}
+
+#[cfg(not(debug_assertions))]
+impl<T> Pretty<T, Report> for Option<T> {
+    fn pretty_msg(self, msg: impl Display) -> Result<T, Report> {
+        self.ok_or(Report::msg(format!("{msg}",)))
+    }
+
+    fn pretty(self) -> Result<T, Report> {
+        self.ok_or(Report::msg("None"))
     }
 }
