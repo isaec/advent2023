6ff19685b50f4529bd6e29648caa19ec107ca7f0
Isaac <isaac@rubenfamily.com>
2023-12-15 17:03:07 -0600

    clean up more

--- day15/src/part2.rs
+++ day15/src/part2.rs
@@ -7,10 +7,10 @@ fn main() {
     dbg!(part2(input));
 }
 
-fn hash_algorithm(input: &str) -> u64 {
+fn hash_algorithm(input: &str) -> usize {
     let mut value = 0;
     for c in input.chars() {
-        let ascii_value = c as u64;
+        let ascii_value = c as usize;
         value += ascii_value;
         value *= 17;
         value %= 256;
@@ -19,58 +19,46 @@ fn hash_algorithm(input: &str) -> u64 {
     value
 }
 
-fn parse(input: &str) -> impl Iterator<Item = (&str, Option<u64>)> {
+fn parse(input: &str) -> impl Iterator<Item = (&str, Option<usize>)> {
     input.split(',').map(|s| s.trim()).map(|s| {
         s.split_once('=')
-            .map(|(l, r)| (l, r.parse::<u64>().ok()))
+            .map(|(l, r)| (l, r.parse::<usize>().ok()))
             .unwrap_or((&s[..s.len() - 1], None))
     })
 }
 
-pub fn part2(input: &str) -> u64 {
+pub fn part2(input: &str) -> usize {
     let parsed = parse(input);
-    let mut boxes: HashMap<u64, Vec<(&str, u64)>> = HashMap::new();
+    let mut boxes: Vec<Vec<(&str, usize)>> = vec![Vec::new(); 256];
     for (label, val) in parsed {
         let hash = hash_algorithm(label);
         dbg!((label, val));
         match val {
             Some(val) => {
-                let entry = boxes.entry(hash).or_insert_with(Vec::new);
-                let mut entry = entry
-                    .iter()
-                    .map(|(l, v)| if *l == label { (*l, val) } else { (*l, *v) })
-                    .collect_vec();
-                if entry.iter().filter(|(l, _)| *l == label).count() == 0 {
-                    entry.push((label, val));
+                if let Some(i) = boxes[hash].iter().position(|(l, _)| *l == label) {
+                    boxes[hash][i] = (label, val);
+                } else {
+                    boxes[hash].push((label, val));
                 }
-                boxes.insert(hash, entry);
             }
             None => {
-                let entry = boxes.entry(hash).or_insert_with(Vec::new);
-                let entry = entry
-                    .iter()
-                    .filter(|(l, _)| *l != label)
-                    .copied()
-                    .collect_vec();
-                boxes.insert(hash, entry);
+                if let Some(i) = boxes[hash].iter().position(|(l, _)| *l == label) {
+                    boxes[hash].remove(i);
+                }
             }
         }
-        // dbg!(&boxes);
     }
 
     boxes
         .iter()
-        .map(|(k, v)| {
-            let box_number = k + 1;
+        .enumerate()
+        .map(|(box_i, v)| {
             v.iter()
                 .enumerate()
-                .map(|(i, (_, v))| {
-                    let slot_number = i as u64 + 1;
-                    box_number * slot_number * v
-                })
-                .sum::<u64>()
+                .map(|(slot_i, (_, v))| (box_i + 1) * (slot_i + 1) * v)
+                .sum::<usize>()
         })
-        .sum::<u64>()
+        .sum::<usize>()
 }
 
 #[cfg(test)]
