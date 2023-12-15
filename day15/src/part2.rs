3584a53984c9993316b5381ed389066779ac953f
Isaac <isaac@rubenfamily.com>
2023-12-14 23:10:16 -0600

    pt1

--- day15/src/part2.rs
+++ day15/src/part2.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use miette::Result;
 use miette_pretty::Pretty;
 use parse::QuickRegex;
@@ -7,13 +8,28 @@ fn main() {
     dbg!(part2(input).unwrap());
 }
 
-fn parse(input: &str) -> Result<Vec<&str>> {
-    input.lines().map(|l| Ok(l)).collect()
+fn hash_algorithm(input: &str) -> u64 {
+    let mut value = 0;
+    for c in input.chars() {
+        let ascii_value = c as u64;
+        value += ascii_value;
+        value *= 17;
+        value %= 256;
+    }
+
+    value
 }
 
-pub fn part2(input: &str) -> Result<i64> {
+fn parse(input: &str) -> Result<Vec<&str>> {
+    Ok(input.split(',').map(|s| s.trim()).collect_vec())
+}
+
+pub fn part2(input: &str) -> Result<u64> {
     let parsed = parse(input)?;
-    Ok(0)
+    Ok(parsed
+        .iter()
+        .map(|s| dbg!(hash_algorithm(dbg!(s))))
+        .sum::<u64>())
 }
 
 #[cfg(test)]
@@ -24,9 +40,15 @@ mod part2_tests {
     #[test]
     fn example() {
         let input = indoc! {r#"
-
+rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
 "#};
-        assert_eq!(part2(input).expect("part2 should return Ok"), 0);
+        assert_eq!(part2(input).expect("part2 should return Ok"), 1320);
+    }
+
+    #[test]
+    fn hash_example() {
+        let input = "HASH";
+        assert_eq!(hash_algorithm(input), 52);
     }
 
     #[test]
