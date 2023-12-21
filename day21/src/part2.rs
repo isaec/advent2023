e3a9bb6b53141fd8e1dbae3e626f3b857d185e44
Isaac <isaac@rubenfamily.com>
2023-12-21 03:14:02 -0600

    "finish" pt2

--- day21/src/part2.rs
+++ day21/src/part2.rs
@@ -24,40 +24,16 @@ fn parse(input: &str) -> Result<G> {
     Tile::parse_grid(input)
 }
 
-pub fn part2(input: &str, steps: u64) -> Result<u64> {
-    let grid = parse(input)?;
-    let starts = grid.lookup(Tile::Start);
-    assert!(starts.len() == 1);
-    let start = starts[0];
-    let mut graph = GraphMap::<(usize, usize), (), Undirected>::new();
-
-    dbg!(steps % grid.width as u64);
-
-    dbg!(start);
-
-    let mut frontier = vec![start];
-
-    while let Some(current) = frontier.pop() {
-        let neighbors = grid.get_neighbors(current.0, current.1)?;
-        for neighbor in neighbors.iter(&parse::Relationship::Orthogonal) {
-            let tile = *grid.get(neighbor.0, neighbor.1)?;
-
-            if tile == Tile::Rock {
-                continue;
-            }
-
-            if tile == Tile::GardenPlot && !graph.contains_node(neighbor) {
-                frontier.push(neighbor);
-                graph.add_edge(current, neighbor, ());
-            }
-        }
-    }
-
+fn reachable_after_n(
+    grid: &Grid<Tile>,
+    graph: &GraphMap<(usize, usize), (), Undirected>,
+    start: (usize, usize),
+    n: u64,
+) -> u64 {
     let mut currently_at_map: HashMap<(isize, isize), HashSet<(usize, usize)>> = HashMap::new();
     currently_at_map.insert((0, 0), [start].iter().copied().collect());
 
-    for step in 0..steps {
-        // dbg!(step);
+    for _ in 0..n {
         let mut next_map = HashMap::new();
         for (tile_coord, currently_at) in currently_at_map.iter() {
             for current_coord in currently_at.iter() {
@@ -65,7 +41,7 @@ pub fn part2(input: &str, steps: u64) -> Result<u64> {
                     .get_neighbors_wrapping(current_coord.0, current_coord.1)
                     .unwrap();
                 for neighbor in neighbors.iter(&parse::Relationship::Orthogonal) {
-                    let tile = *grid.get(neighbor.0, neighbor.1)?;
+                    let tile = *grid.get(neighbor.0, neighbor.1).unwrap();
 
                     if tile == Tile::Rock {
                         continue;
@@ -103,36 +79,61 @@ pub fn part2(input: &str, steps: u64) -> Result<u64> {
             }
         }
         currently_at_map = next_map;
-        pattern_enum! {
-            enum At {
-                At = "O",
-                Garden = ".",
-                Rock = "#",
+    }
+
+    currently_at_map
+        .iter()
+        .map(|(_, currently_at)| currently_at.len())
+        .sum::<usize>() as u64
+}
+
+pub fn part2(input: &str, steps: u64) -> Result<u64> {
+    let grid = parse(input)?;
+    let starts = grid.lookup(Tile::Start);
+    assert!(starts.len() == 1);
+    let start = starts[0];
+    let mut graph = GraphMap::<(usize, usize), (), Undirected>::new();
+
+    dbg!(grid.width);
+    dbg!(steps % grid.width as u64);
+
+    dbg!(start);
+
+    let mut frontier = vec![start];
+
+    while let Some(current) = frontier.pop() {
+        let neighbors = grid.get_neighbors(current.0, current.1)?;
+        for neighbor in neighbors.iter(&parse::Relationship::Orthogonal) {
+            let tile = *grid.get(neighbor.0, neighbor.1)?;
+
+            if tile == Tile::Rock {
+                continue;
             }
-        }
-        if step == 65 {
-            grid.map(|(coord, tile)| {
-                if currently_at_map.get(&(0, 0)).unwrap().contains(&coord) {
-                    At::At
-                } else {
-                    match tile {
-                        Tile::GardenPlot => At::Garden,
-                        Tile::Rock => At::Rock,
-                        Tile::Start => At::Garden,
-                    }
-                }
-            })
-            .debug_to_file("f1")?;
-            dbg!(step);
-            dbg!(steps % step);
-            break;
+
+            if tile == Tile::GardenPlot && !graph.contains_node(neighbor) {
+                frontier.push(neighbor);
+                graph.add_edge(current, neighbor, ());
+            }
         }
     }
 
-    Ok(currently_at_map
+    let n = start.0 as u64;
+
+    let mut vals = vec![];
+    for i in 0..3 {
+        let at = n + i * grid.width as u64;
+        let val = reachable_after_n(&grid, &graph, start, at);
+        vals.push((at, val));
+    }
+
+    dbg!(vals
         .iter()
-        .map(|(_, currently_at)| currently_at.len())
-        .sum::<usize>() as u64)
+        .map(|(at, val)| format!("{{ {at}, {val} }}"))
+        .join(", "));
+
+    // https://www.wolframalpha.com/input?i=quadratic+regression&assumption=%7B%22F%22%2C+%22QuadraticFitCalculator%22%2C+%22data2%22%7D+-%3E%22%7B%7B+65%2C+3791+%7D%2C+%7B+196%2C+33646+%7D%2C+%7B+327%2C+93223+%7D%7D%22
+
+    todo!()
 }
 
 #[cfg(test)]
