a6d8009ab6225c89acd1c61287739521f0edd178
Isaac <isaac@rubenfamily.com>
2023-12-22 23:34:27 -0600

    pt1

--- day23/src/part1.rs
+++ day23/src/part1.rs
@@ -1,19 +1,70 @@
+use itertools::Itertools;
 use miette::Result;
 use miette_pretty::Pretty;
 use parse::{Grid, QuickRegex, Tile};
+use petgraph::{algo::all_simple_paths, graphmap::GraphMap, Directed};
 
 fn main() {
     let input = include_str!("../input.txt");
     dbg!(part1(input).unwrap());
 }
 
-fn parse(input: &str) -> Result<Vec<&str>> {
-    input.lines().map(|l| Ok(l)).collect()
+Tile! {
+    Path = '.',
+    Forest = '#',
+    SlopeUp = '^',
+    SlopeDown = 'v',
+    SlopeLeft = '<',
+    SlopeRight = '>',
 }
 
-pub fn part1(input: &str) -> Result<i64> {
-    let parsed = parse(input)?;
-    Ok(0)
+fn parse(input: &str) -> Result<Grid<Tile>> {
+    Tile::parse_grid(input)
+}
+
+pub fn part1(input: &str) -> Result<u64> {
+    let grid = parse(input)?;
+    let graph: GraphMap<(usize, usize), (), Directed> = grid.build_graph(
+        &parse::Relationship::Orthogonal,
+        |(f_tile, f_coord), (t_tile, t_coord)| match (f_tile, t_tile) {
+            (Tile::Path, Tile::Path) => Some(()),
+            (Tile::Forest, _) => None,
+            (_, Tile::Forest) => None,
+            (_, Tile::Path) => match (
+                f_tile,
+                f_coord.0 as isize - t_coord.0 as isize,
+                f_coord.1 as isize - t_coord.1 as isize,
+            ) {
+                (Tile::SlopeUp, 0, 1) => Some(()),
+                (Tile::SlopeDown, 0, -1) => Some(()),
+                (Tile::SlopeLeft, 1, 0) => Some(()),
+                (Tile::SlopeRight, -1, 0) => Some(()),
+                _ => None,
+            },
+            (Tile::Path, _) => Some(()),
+            _ => None,
+        },
+    );
+
+    let start = grid
+        .lookup(Tile::Path)
+        .iter()
+        .min_by_key(|coord| coord.1)
+        .unwrap()
+        .clone();
+
+    let end = grid
+        .lookup(Tile::Path)
+        .iter()
+        .max_by_key(|coord| coord.1)
+        .unwrap()
+        .clone();
+
+    dbg!(start, end);
+
+    let paths = all_simple_paths::<Vec<_>, _>(&graph, start, end, 0, None).collect_vec();
+    dbg!(paths.len());
+    paths.iter().map(|p| p.len() as u64 - 1).max().pretty()
 }
 
 #[cfg(test)]
@@ -24,9 +75,31 @@ mod part1_tests {
     #[test]
     fn example() {
         let input = indoc! {r#"
-
+#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
 "#};
-        assert_eq!(part1(input).expect("part1 should return Ok"), 0);
+        assert_eq!(part1(input).expect("part1 should return Ok"), 94);
     }
 
     #[test]
